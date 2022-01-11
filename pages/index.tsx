9b14fbb939bba58b82fd29241a8b6a21436faa9f
saqla <whitecross8fortune8fatedress@gmail.com>
2022-01-11 21:44:58 +0900

    🔥

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 
+const foo = "foo"
+let bar = foo
+
 const Home: NextPage = () => {
-  return (
-    <div>test</div>
-  )
+  return <div>test</div>
 }
 
 export default Home
